f860a93c5c8f1b5f156c8137c0ec3a375e11d807
MisksHatesNumberBlocks <mb3096767@gmail.com>
2025-12-04 09:50:07 -0300

    Remove unused expression evaluation functions
    Removed evalExpression and defineExpression functions from Common class.

--- expressions/strings.lua
+++ expressions/strings.lua
@@ -8,38 +8,6 @@ Expression = {
    },
 }
 
-function Common:evalExpression(maybeExpression, actorId, context, paramSpec)
-   local result
-   local typ = type(maybeExpression)
-   if typ == "table" and Expression.expressions[maybeExpression.expressionType] then
-      result = Expression.expressions[maybeExpression.expressionType].eval(self, maybeExpression, actorId, context)
-   else
-      -- not an expression, so treat as a primitive
-      result = maybeExpression
-   end
-
-   -- if provided, validate result according to spec
-   if paramSpec ~= nil then
-      if paramSpec.props ~= nil then
-         if paramSpec.props.min ~= nil and result < paramSpec.props.min then
-            result = paramSpec.props.min
-         end
-         if paramSpec.props.max ~= nil and result > paramSpec.props.max then
-            result = paramSpec.props.max
-         end
-         if paramSpec.props.discrete ~= nil then
-            result = math.floor(result)
-         end
-      end
-   end
-
-   return result
-end
-
-function Common:defineExpression(expressionType, expression)
-   Expression.expressions[expressionType] = util.deepCopyTable(expression)
-end
-
 Expression.expressions["join strings"] = {
    returnType = "string",
    description = "join two strings",
